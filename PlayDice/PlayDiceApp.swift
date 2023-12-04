78235493eef07a0ec2531c97087297907e8a768d
Tony Santiago <85644672+t0nysantiago@users.noreply.github.com>
2023-12-04 18:35:42 -0300

    Initial Commit

---
+++ PlayDice/PlayDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayDiceApp.swift
+//  PlayDice
+//
+//  Created by Tony Santiago on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
